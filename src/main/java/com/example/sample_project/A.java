09d104c1bcd1eaf03368ca766536a83c53a07ada
Vaibhav Rawade <vaibhavrawade123@gmail.com>
2025-12-29 21:29:13 -1200

    done

--- src/main/java/com/example/sample_project/A.java
+++ src/main/java/com/example/sample_project/A.java
@@ -1,5 +1,14 @@
 package com.example.sample_project;
 
-public class A {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class A 
+{
+	@GetMapping
+    public String getNamde()
+	{
+        return "vaibhav rawade learning devops";
+    }
 }
